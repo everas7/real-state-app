8c227e8ddca2b91b8168e42086b8a5be8db2a3a4
Enmanuel Veras Vargas <enmaveras07@gmail.com>
2021-05-19 04:46:42 -0400

    Enhances login

--- webapp/src/features/access/pages/AccessPage.tsx
+++ webapp/src/features/access/pages/AccessPage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import Login from '../components/Login/Login';
 import Signup from '../components/Signup/Signup';
+import styles from './AccessPage.module.scss';
 
 export default function AccessPage() {
   const [login, setLogin] = useState(true);
@@ -11,16 +12,22 @@ export default function AccessPage() {
   }
 
   return (
-    <Container className="h-100">
-      <Row className="h-100 justify-content-center align-items-center">
-        <Col md="6">
-          {login ? (
-            <Login onSwitch={handleSwitch} />
-          ) : (
-            <Signup onSwitch={handleSwitch} />
-          )}
-        </Col>
-      </Row>
-    </Container>
+    <div className={styles['access-page']}>
+      <div className={styles['access-page__image']}/>
+
+      <div className={styles['access-page__page']}>
+        <Container className="h-100">
+          <Row className="h-100 justify-content-center align-items-center">
+            <Col md="6">
+              {login ? (
+                <Login onSwitch={handleSwitch} />
+              ) : (
+                <Signup onSwitch={handleSwitch} />
+              )}
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
   );
 }
