80c61f21daf8bcfad457734c65ba0ca1e87a725c
Enmanuel Veras Vargas <enmaveras07@gmail.com>
2021-05-19 16:05:25 -0400

    Fixes seeders

--- api/src/db/seeders/20210513230903-demo-users.js
+++ api/src/db/seeders/20210513230903-demo-users.js
@@ -4,34 +4,10 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('users', [
       {
-        name: 'John Client',
-        email: 'jclient@test.com',
-        roleId: 3,
-        password: await bcrypt.hash('klkconklk1', 10),
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'Mary Realtor',
-        email: 'mrealtor@test.com',
-        roleId: 2,
-        password: await bcrypt.hash('klkconklk1', 10),
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'Ryan Realtor',
-        email: 'rrealtor@test.com',
-        roleId: 2,
-        password: await bcrypt.hash('klkconklk1', 10),
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'Peter Admin',
-        email: 'padmin@test.com',
+        name: 'John Doe',
+        email: 'jdoe@test.com',
         roleId: 1,
-        password: await bcrypt.hash('klkconklk1', 10),
+        password: await bcrypt.hash('strongpassword1', 10),
         createdAt: new Date(),
         updatedAt: new Date(),
       },
